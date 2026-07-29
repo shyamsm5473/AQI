95bd570dede6c0f79bd6013248c4a753d533ac56
shyamsm5473 <shyamsm5473@gmail.com>
2026-07-29 21:42:11 +0530

    Update settings.py

--- iiitl_project/settings.py
+++ iiitl_project/settings.py
@@ -30,9 +30,10 @@ environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 SECRET_KEY = env('SECRET_KEY', default='django-insecure-placeholder-key-for-build')
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
